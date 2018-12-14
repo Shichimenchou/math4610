aca8a2e71802bf13de975823346eddecea610f79
Phillip Linson <35586624+Shichimenchou@users.noreply.github.com>
2018-12-13 21:31:45 -0700

    Update hw3-4.cpp

--- HW3/Problem4/hw3-4.cpp
+++ HW3/Problem4/hw3-4.cpp
@@ -50,10 +50,10 @@ double oneNormMat(vector<vector<double>> A, int n, int m)
 	double ans = 0.0;
 	double colSum = 0.0;
 
-	for(int i = 0; i < n; i++)
+	for(int i = 0; i < m; i++)
 	{
 		colSum = 0.0;
-		for(int j = 0; j < m; j++)
+		for(int j = 0; j < n; j++)
 		{
 			colSum += A[j][i];
 		}
@@ -69,8 +69,21 @@ double oneNormMat(vector<vector<double>> A, int n, int m)
 
 int main()
 {
+	vector<vector<double>> A;
 	
-
+	for(int i = 0; i < 5; i++)
+	{
+		vector<double> row;
+		for(int j = 0; j < 4; j++)
+		{
+			row.push_back(i);
+		}
+		A.push_back(row);
+	}
+	
+	cout << "The Frobenius Norm of A is: " << frobNormMat(A, 5, 3) << endl;
+	cout << "The Infinity Norm of A is: " << infNormMat(A, 5, 3) << endl;
+	cout << "The One Norm of A is: " << oneNormMat(A, 5, 3) << endl;
 
 	return 0;
 }
